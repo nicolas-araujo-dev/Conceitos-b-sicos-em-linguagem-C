0f9ac683a6d44562aef327b7d19b27a3cf977a3f
Nicolas Araujo Coelho <nicolas.araujocoelho@gmail.com>
2025-08-26 10:59:14 -0300

    Add files via upload

---
+++ Aula01.c
@@ -0,0 +1,37 @@
+// Usamos o printf em C para dar instrução ao código // 
+// Comentario para várias linhas //
+/* Comentario para um aunica linha */
+// Em linguagem C é sempre necessario ao final do código o ; //
+// A estrutura em C é sempre utilizado o int main (void){ return 0; }
+
+// Atividade em sala de aula dia 25/08 // 
+
+int main (void){
+
+    printf ("Calculando area e a base de um triânguo")
+
+    float base,altura,resultado; 
+    base = 5.0; 
+    altura = 3.0;
+    resultado = (base*altura)/2;
+    printf ("A área do triângulo é: %2.f\n", resultado); 
+    return 0;  
+
+    float base, altura, area; 
+    /* o & e comercial, é usado para p computador buscar o valor dentro da memória */
+    print("Digite o valor da base:")
+    scranf("%f, &base"); /* scranf é usado como o input no Python */ 
+    printf("Digite a altura:"); 
+    scranf ("%f", altura)/2;
+
+    printf("O valor da área calculado é de:%.2f/n," area); 
+    return 0 
+
+    // Realizando calcúlos // 
+
+    int x, y, z; 
+    x = 5;
+    printf("O valor de x++ é: %d", x++); // o ++ e -- são incrementos 
+    prinf("O valor de x é: %d",x); 
+    return 0;
+} 
